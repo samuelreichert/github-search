d614c2966c16f8be83c1c12e7621b7e104f72c84
Samuel Reichert <samuelreichertt@gmail.com>
2020-05-30 19:44:16 -0300

    Build api method to search for repositories

--- src/api/index.js
+++ src/api/index.js
@@ -1,2 +1,19 @@
-const BASE_URL = 'https://api.github.com/search/repositories'
-// ?q=countries-app&per_page=10&page=2
+import axios from 'axios'
+
+const BASE_URL = `${process.env.REACT_APP_GITHUB_URL}/search/repositories`
+
+// ?q=react&per_page=10&page=1
+export const searchRepositories = async ({ search, page = 1, perPage = 10 } = {}) => {
+  try {
+    const params = {
+      q: search,
+      page,
+      per_page: perPage
+    }
+
+    const { data } = await axios.get(BASE_URL, { params })
+    return data
+  } catch (error) {
+    console.error('Error trying to search repositories from GitHub', error)
+  }
+}
