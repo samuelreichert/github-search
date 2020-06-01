e65fa06e684fab26b504e225ccb9b891462ebb93
Samuel Reichert <samuelreichertt@gmail.com>
2020-05-31 23:46:02 -0300

    Add header tag to Navbar component

--- src/modules/Navbar/index.js
+++ src/modules/Navbar/index.js
@@ -1,17 +1,19 @@
 import React from 'react'
 
 const Navbar = () => (
-  <nav className='navbar navbar-dark bg-primary'>
-    <div className='container'>
-      <a className='navbar-brand' href='https://samuelreichert.github.io' rel='noopener noreferrer' target='_blank'>Samuel Reichert</a>
+  <header>
+    <nav className='navbar navbar-dark bg-primary'>
+      <div className='container'>
+        <a className='navbar-brand' href='https://samuelreichert.github.io' rel='noopener noreferrer' target='_blank'>Samuel Reichert</a>
 
-      <ul className='navbar-nav ml-auto'>
-        <li className='nav-item'>
-          <a className='nav-link' href='https://saltoks.com/team' rel='noopener noreferrer' target='_blank'>SaltoKS</a>
-        </li>
-      </ul>
-    </div>
-  </nav>
+        <ul className='navbar-nav ml-auto'>
+          <li className='nav-item'>
+            <a className='nav-link' href='https://saltoks.com/team' rel='noopener noreferrer' target='_blank'>SaltoKS</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
 )
 
 export default Navbar
