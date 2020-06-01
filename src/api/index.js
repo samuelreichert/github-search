1b7110e7d894e49cfb182ee07f63770abe25b3cf
Samuel Reichert <samuelreichertt@gmail.com>
2020-06-01 00:09:32 -0300

    Create new method to try to authenticate a github user

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,8 @@
 import axios from 'axios'
 
-const BASE_URL = `${process.env.REACT_APP_GITHUB_URL}/search/repositories`
-
-// ?q=react&per_page=10&page=1
 export const searchRepositories = async ({ search, page = 1, perPage = 10 } = {}) => {
+  const BASE_URL = `${process.env.REACT_APP_GITHUB_URL}/search/repositories`
+
   try {
     const params = {
       q: search,
@@ -13,6 +12,25 @@ export const searchRepositories = async ({ search, page = 1, perPage = 10 } = {}
 
     const { data } = await axios.get(BASE_URL, { params })
     return data
+  } catch (error) {
+    console.error('Error trying to search repositories from GitHub', error)
+  }
+}
+
+export const postUserCode = async (code) => {
+  const AUTH_URL = `${process.env.REACT_APP_AUTH_URL}/access_token`
+
+  try {
+    const params = {
+      client_id: process.env.REACT_APP_CLIENT_ID,
+      client_secret: process.env.REACT_APP_CLIENT_SECRET,
+      code: code,
+      redirect_uri: 'http://localhost:3000/'
+    }
+
+    const teste = await axios.post(AUTH_URL, params)
+    console.log(teste)
+    return teste
   } catch (error) {
     console.error('Error trying to search repositories from GitHub', error)
   }
